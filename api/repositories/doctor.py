0ffb9c693bad5d6d6bc52e83dd26aef5b7ee33d8
Dmitry Grishin <kenpxrkasf@gmail.com>
2024-05-14 14:32:36 +0300

    added exc handler

--- api/repositories/doctor.py
+++ api/repositories/doctor.py
@@ -6,6 +6,7 @@ from api.models.doctor import DoctorModel
 from .repository import SQLAlchemyRepository
 from api.models.days_off import DayOffModel
 from api.schemas.days_off import ReadDaysOffDTO
+from sqlalchemy.exc import IntegrityError
 
 
 class DoctorRepository(SQLAlchemyRepository):
@@ -15,13 +16,16 @@ class DoctorRepository(SQLAlchemyRepository):
     async def create_days_off(self, days_off_data: dict, id: int | UUID, session: AsyncSession) -> list[datetime.date]:
         new_days_off = []
         for day in days_off_data["days_off_date"]:
-            new_day_off = DayOffModel(
-                doctor_id = id,
-                days_off_date = day
-            )
-            session.add(new_day_off)
-            await session.flush()
-            await session.commit()
+            try:
+                new_day_off = DayOffModel(
+                    doctor_id = id,
+                    days_off_date = day
+                )
+                session.add(new_day_off)
+                await session.flush()
+                await session.commit()
+            except IntegrityError:
+                continue
             new_days_off.append(new_day_off.days_off_date)
         return new_days_off
 
