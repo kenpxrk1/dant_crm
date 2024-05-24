88ee6faba335ca9e6eccead0b962cb41315d6014
Dmitry Grishin <kenpxrkasf@gmail.com>
2024-05-24 13:36:19 +0300

    cors added

--- api/main.py
+++ api/main.py
@@ -2,6 +2,7 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from api.handlers.routers import routers
 from api.dependencies import user_service, user_repo
+from fastapi.middleware.cors import CORSMiddleware
 
 
 @asynccontextmanager
@@ -12,6 +13,21 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(title="CRM DANTIST", lifespan=lifespan)
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 for router in routers:
     app.include_router(router)
